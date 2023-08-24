c2c6844ee970c3f77db6ae2bccacd0c56626ee53
gmarvis <sgmarvis@gmail.com>
2023-08-24 14:23:38 +0100

    add some html and css

---
+++ index.js
@@ -0,0 +1,9 @@
+const WordOne = document.getElementById('word-one')
+const WordTwo = document.getElementById('word-two')
+const btn = document.getElementById('btn')
+
+
+btn.addEventListener("click", (event)=>{
+    event.preventDefault()
+    console.log("this is woking well");
+})
